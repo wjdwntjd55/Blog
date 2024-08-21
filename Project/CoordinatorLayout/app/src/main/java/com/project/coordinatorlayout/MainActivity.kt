56a519af8c4f60194f6b927a2b16c56c8b151b2d
juseong <wjdwntjd55@naver.com>
2024-08-21 09:53:46 +0900

    feat : RecyclerView 설정

--- Project/CoordinatorLayout/app/src/main/java/com/project/coordinatorlayout/MainActivity.kt
+++ Project/CoordinatorLayout/app/src/main/java/com/project/coordinatorlayout/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.project.coordinatorlayout
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.project.coordinatorlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        adapter = MainAdapter()
+        binding.recyclerViewMain.adapter = adapter
+        binding.recyclerViewMain.layoutManager = LinearLayoutManager(this)
+
+        val items = List(20) { "${it + 1}번째 아이템" }
+        adapter.submitList(items)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
