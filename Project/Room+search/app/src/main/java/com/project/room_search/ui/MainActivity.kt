e036e56ef7911347e81756114c95a16e0a4519fb
juseong <wjdwntjd55@naver.com>
2024-08-28 16:25:25 +0900

    feat : 검색어 입력했을 때, 데이터 추가하는 기능 구현

--- Project/Room+search/app/src/main/java/com/project/room_search/ui/MainActivity.kt
+++ Project/Room+search/app/src/main/java/com/project/room_search/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.project.room_search.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.room_search.app.MyApplication
+import com.project.room_search.data.local.entities.Search
 import com.project.room_search.data.repository.MainRepository
 import com.project.room_search.databinding.ActivityMainBinding
 
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         init()
         observeData()
+        inputSearch()
     }
 
     private fun init() {
@@ -49,5 +52,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun inputSearch() {
+        // EditText에서 엔터 키 이벤트 처리
+        binding.editTextMain.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val searchText = binding.editTextMain.text.toString().trim()
+                if (searchText.isNotEmpty()) {
+                    // 입력한 검색어를 ViewModel에 추가
+                    mainViewModel.insertSearch(Search(contents = searchText)) // Search 객체 생성
+                    binding.editTextMain.text?.clear() // 입력 필드 초기화
+                }
+                true
+            } else {
+                false
+            }
+        }
     }
 }
