169740bb9ccab4b756692c8fa9631f48ebcf0cae
juseong <wjdwntjd55@naver.com>
2024-09-04 16:08:54 +0900

    feat : photoview 사용해서 이미지 확대, 축소

--- Project/enlargeImage/app/build.gradle.kts
+++ Project/enlargeImage/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // PhotoView
+    implementation("io.getstream:photoview:1.0.2")
+    // glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- Project/enlargeImage/app/src/main/java/com/project/enlargeimage/MainActivity.kt
+++ Project/enlargeImage/app/src/main/java/com/project/enlargeimage/MainActivity.kt
@@ -2,10 +2,23 @@ package com.project.enlargeimage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.project.enlargeimage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val photoView = binding.photoView
+        Glide.with(this)
+            .load("https://edgio.clien.net/F01/14662322/1223d7c7cb74ee.jpg?scale=width[740],options[limit]")
+            .into(photoView)
+
     }
 }
