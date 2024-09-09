4de492fb53261efa82939345be56152024f8f9fd
juseong <wjdwntjd55@naver.com>
2024-09-09 17:22:58 +0900

    feat : 이용약관 동의 화면 ui 구현

--- Project/AgreeToTermsOfUse/app/src/main/java/com/project/agreetotermsofuse/MainActivity.kt
+++ Project/AgreeToTermsOfUse/app/src/main/java/com/project/agreetotermsofuse/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.project.agreetotermsofuse
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.project.agreetotermsofuse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this
+    }
+
+    fun openBottomSheet() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_layout)
+
+        bottomSheetDialog.show()
     }
 }
