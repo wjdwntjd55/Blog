32d294ab00aef3a544428a6a1023a49a8e68a87e
juseong <wjdwntjd55@naver.com>
2024-10-11 15:20:08 +0900

    feat : Data class 구현

---
+++ Project/Paging_Network/app/src/main/java/com/project/paging_network/data/mode/GithubResponse.kt
@@ -0,0 +1,3 @@
+package com.project.paging_network.data.mode
+
+class GithubResponse : ArrayList<GithubResponseItem>()
---
+++ Project/Paging_Network/app/src/main/java/com/project/paging_network/data/mode/License.kt
@@ -0,0 +1,9 @@
+package com.project.paging_network.data.mode
+
+data class License(
+    val key: String,
+    val name: String,
+    val node_id: String,
+    val spdx_id: String,
+    val url: String
+)
